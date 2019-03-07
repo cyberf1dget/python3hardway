1c5769b229a1e06a722f3d4e6cb8975a173f5f29
cyberf1dget <fidget@cyberf1dget.com>
2019-03-07 02:45:46 -0500

    Exercise 3

---
+++ ex3.py
@@ -0,0 +1,30 @@
+# Print a string
+print("I will now count my chickens:")
+
+# Print a string, then calculates 25 plus result of 30/6
+print("Hens", 25 + 30 / 6)
+
+# Print a string, then calculates remainder of (25*3)/4 and subtracts it from 100
+print("Roosters", 100 - 25 * 3 % 4)
+
+# Print a string
+print("Now I will count the eggs:")
+ 
+# Calculates remainder of (4/2) - (1/4) and sums that with 6
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# Print a string, then prints boolean of 5<-2
+print("Is it true that 3 + 2 < 5 - 7?")
+print( 3 + 2 < 5 - 7)
+
+# Print a string, then calculates 3+2 and 5-7
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+print("Oh, that's why it's False.")
+print("How about some more.")
+
+# Print a string, then performs comparison operation and prints whether statement
+# true or false.
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
